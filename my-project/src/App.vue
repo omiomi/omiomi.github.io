805c363ce0383548955dfb3d52e00b60ffc3f123
19534212@qq.com <hxfq5166>
2018-01-19 22:29:24 +0800

    ggg

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,23 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav-top></nav-top>
     <router-view/>
   </div>
 </template>
 
 <script>
+import navTop from './components/TopNav'
+
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    navTop
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+  @import "node_modules/bootstrap/scss/functions";
+  @import "node_modules/bootstrap/scss/variables";
+  @import "node_modules/bootstrap/scss/mixins";
+
+  // Optional
+  @import "node_modules/bootstrap/scss/reboot";
+  @import "node_modules/bootstrap/scss/type";
+  @import "node_modules/bootstrap/scss/images";
+  @import "node_modules/bootstrap/scss/code";
+  @import "node_modules/bootstrap/scss/grid";
+  $colors: (
+    "blue": $blue,
+    "indigo": $indigo,
+    "purple": $purple,
+    "pink": $pink,
+    "red": $red,
+    "orange": $orange,
+    "yellow": $yellow,
+    "green": $green,
+    "teal": $teal,
+    "cyan": $cyan,
+    "white": $white,
+    "gray": $gray-600,
+    "gray-dark": $gray-800
+  ) !default;
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #{$pink};
 }
 </style>
---
+++ my-project/src/components/TopNav.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="nav-top">
+    <div class="header-inner-9">
+      <div class="logo-9">
+        <a href="#" class="innotree-logo">
+        </a>
+      </div>
+      <div class="nav-9">
+        <ol>
+          <li class="snl"><a href="#">首页</a></li>
+          <li class="snl"><a href="#">因果资讯</a></li>
+          <li class="snl"><a href="#">创投榜单</a></li>
+          <li class="snl"><a href="#">摆谱</a></li>
+          <li class="snl"><a href="#">值说</a></li>
+        </ol>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topNav'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  .nav-top{
+    width: 100%;
+    height: 64px;
+    background: #fff;
+    border-bottom: 1px solid #eaeaea;
+    z-index: 1002;
+    position: relative;
+    .header-inner-9{
+      position: relative;
+      margin: 0 auto;
+      width: 1140px;
+      height: 64px;
+      .logo-9{
+        img{
+          width: 106px;
+          height: auto;
+          position: absolute;
+          left: 0;
+          top:12px;
+        }
+      }
+    }
+    .nav-9{
+      width: 600px;
+      height: 64px;
+      position: absolute;
+      left: 540px;
+      top:0;
+      ol{
+        list-style: none;
+        margin: 0;
+        li{
+          display: inline-block;
+          line-height: 64px;
+          position: relative;
+          height: 64px;
+          a{
+            color: #000;
+            font-size: 18px;
+            display: inline-block;
+            position: relative;
+            padding: 0 12px;
+            text-decoration: none;
+
+          }
+          &:hover a,
+          &.on a{
+            color: #19ae66;
+          }
+          &.on:after{
+            content: "";
+            height: 2px;
+            width: 100%;
+            display: inline-block;
+            position: absolute;
+            background: #19ae66;
+            bottom: 0;
+            left: 0;
+          }
+        }
+
+      }
+    }
+  }
+
+</style>
